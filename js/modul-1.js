f6e2890fc61ce3ec6649edcbac48782c2ec8758c
Fedorenkot777 <155662260+Fedorenkot777@users.noreply.github.com>
2024-03-17 21:33:51 +0100

    edit1

---
+++ js/modul-1.js
@@ -0,0 +1,16 @@
+//let userName;
+//console.log(typeof userName);
+
+//userName = "Frilanser in live";
+//console.log(typeof userName);
+
+    //let userName = null;
+   //console.log(userName);
+
+   //let willYouMarryyMe = false;
+   //if (willYouMarryyMe) {
+  //  console.log(':)');
+//} else {console.log(':)');}
+
+
+
