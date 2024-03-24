36d50b1759aa6f25c016bd326b882f9ccee29bf6
Fedorenkot777 <155662260+Fedorenkot777@users.noreply.github.com>
2024-03-24 17:07:00 +0100

    edit3

--- js/modul-2.js
+++ js/modul-2.js
@@ -1,12 +1,22 @@
 
-const answer = prompt("Yka ofiziina nazva JavaScript");
-console.log(answer);
-function answerCheck(string) {
-    if(string === "ECMAScript!") {
-   alert("Birno!");
-        } else {
-            alert("Ne znaete? ECMAScript");
-        }
-    } 
-    
-    answerCheck(answer);
+//const answer = prompt("Yka ofiziina nazva JavaScript");
+//console.log(answer);
+//function answerCheck(string) {
+    //if(string === "ECMAScript!") {
+  // alert("Birno!");
+ //       } else {
+ //           alert("Ne znaete? ECMAScript");
+ //       }
+ //   } 
+  //  answerCheck(answer);
+
+//const time = +prompt("vvedit kilkist xvilin:");
+//console.log(time);
+//function converTime(number) {
+ // const hours = String(Math.floor(number / 60)).padStart(2,0);
+//  console.log(hours);
+//  const minutes = String(number % 60).padStart(2, 0);
+//  console.log(minutes);
+//  console.log(`${hours} : ${minutes}`);
+// } //
+converTime(time);
