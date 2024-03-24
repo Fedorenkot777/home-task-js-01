e58938d447623d4c2951382211b4290c91687a5d
Fedorenkot777 <155662260+Fedorenkot777@users.noreply.github.com>
2024-03-24 16:31:21 +0100

    edit2

---
+++ js/modul-2.js
@@ -0,0 +1,12 @@
+
+const answer = prompt("Yka ofiziina nazva JavaScript");
+console.log(answer);
+function answerCheck(string) {
+    if(string === "ECMAScript!") {
+   alert("Birno!");
+        } else {
+            alert("Ne znaete? ECMAScript");
+        }
+    } 
+    
+    answerCheck(answer);
