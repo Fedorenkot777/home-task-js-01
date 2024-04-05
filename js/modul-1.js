a1f5c33189a67440cc0b395071711569ee16a2a0
Fedorenkot777 <155662260+Fedorenkot777@users.noreply.github.com>
2024-04-05 21:43:27 +0200

    edit 7

--- js/modul-1.js
+++ js/modul-1.js
@@ -12,5 +12,50 @@
   //  console.log(':)');
 //} else {console.log(':)');}
 
+//showMessage();
+//function showMessage() {
+  //console.log("Soobwenie");
+//}
+//showMessage();
 
+//if(2 > 1) {
+//function getSumm() {
+//  let numOne, numTwo;
+//  function getNumOne() {
+//    numOne = 1;
+//  }
+//  function getNumTwo()  {
+ //   numTwo = 2;
+//  }
+//  getNumOne();
+// getNumTwo();
+ // let numSumm = numOne + numTwo;
+ // console.log(numSumm);
+ // }
+//  }
+//getSumm();
 
+//function calcSumm(numOne = 1, numTwo = 2) {
+ // numTwo = numTwo * 2;
+ // console.log(`Peremennay numOne:${numOne}`);
+//  console.log(`Peremennay numTwo:${numTwo}`);
+//  let numSumm = numOne + numTwo;
+//  console.log(`symma:${numSumm}`);
+//}
+//calcSumm(5, 10);
+
+//function calcSumm(numOne, numTwo, more, less) {
+// let numSumm = numOne + numTwo;
+// if (numSumm > 3) {
+//   more();
+ // } else {
+ //   less();
+ // }
+//}
+//function showMoreMessage() {
+ // console.log(`Bolwe yem 3`);
+//}
+//function showLessMessage() {
+//  console.log(`Menwe yem 3`);
+//}
+//calcSumm(1, 5, showMoreMessage, showLessMessage);
